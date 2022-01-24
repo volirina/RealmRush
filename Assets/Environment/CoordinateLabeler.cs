3c1cc1d6d72b340f03f8def823540db7a050e27e
volirina <74723644+volirina@users.noreply.github.com>
2022-01-23 23:44:35 -0500

    BreadthFirstSearch, pathfinding
    Pathfinding, BreadFirstSearch, exploring neighbors

--- Assets/Environment/CoordinateLabeler.cs
+++ Assets/Environment/CoordinateLabeler.cs
@@ -10,17 +10,19 @@ public class CoordinateLabeler : MonoBehaviour
 {
     [SerializeField] Color defaultColor = Color.white;
     [SerializeField] Color blockedColor = Color.gray;
+    [SerializeField] Color exploredColor = Color.yellow;
+    [SerializeField] Color pathColor = new Color(1f, 0.5f, 0f);
 
     TextMeshPro label;
     Vector2Int coordinates = new Vector2Int();
-    Waypoint waypoint;
+    GridManager gridManager;
 
     void Awake()
     {
+        gridManager = FindObjectOfType<GridManager>();
         label = GetComponent<TextMeshPro>();
         label.enabled = false;
-
-        waypoint = GetComponentInParent<Waypoint>();
+        
         DisplayCoordinates();
     }
     void Update()
@@ -45,13 +47,26 @@ public class CoordinateLabeler : MonoBehaviour
 
     void SetLabelColor()
     {
-        if(waypoint.IsPlaceable)
+        if(gridManager == null) { return; }
+
+        Node node = gridManager.GetNode(coordinates);
+        if(node == null) { return; }
+
+        if(!node.isWalkable)
         {
-            label.color = defaultColor;
+            label.color = blockedColor;
+        }
+        else if(node.isPath)
+        {
+            label.color = pathColor;
+        }
+        else if(node.isExplored)
+        {
+            label.color = exploredColor;
         }
         else
         {
-            label.color = blockedColor;
+            label.color = defaultColor;
         }
     }
 
